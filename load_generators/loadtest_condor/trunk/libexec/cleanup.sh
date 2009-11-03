76950c43d0da269272c8591fd2fd2b304971b092
sfiligoi <igor-sfiligoi@users.noreply.github.com>
2009-11-03 22:52:08 +0000

    Add completed.log for easier assessment of the result

--- load_generators/loadtest_condor/trunk/libexec/cleanup.sh
+++ load_generators/loadtest_condor/trunk/libexec/cleanup.sh
@@ -18,9 +18,12 @@ RunID="$2"
 source "$3"
 result="$4"
 
+JobMessage="Fail"
+
 if [ "$result" -eq 0 ]; then
     # no need for error files if everything worked fine
     rm -f "${WorkDir}/job.err.${RunID}"
+    JobMessage="OK"
 fi
 
 if [ "${CreateOutFile}" -eq 1 ]; then
@@ -38,17 +41,21 @@ if [ "${CreateOutFile}" -eq 1 ]; then
 	    else
 		# corrupted file
 		result=10
-		echo "Corrupted file in $RunId" >> fileproblems.log
+		echo "Corrupted file in $RunID" >> fileproblems.log
+		JobMessage="Corrupted"
 	    fi
 	else
 	    # file missing
 	    result=10
-	    echo "Missing file in $RunId" >> fileproblems.log
+	    echo "Missing file in $RunID" >> fileproblems.log
+	    JobMessage="Missing"
 	fi
     else
 	# failed jobs output does not make sense in any case
 	rm -f "${WorkDir}/${OutFileName}.md5" "${WorkDir}/${OutFileName}"
     fi
 fi
+
+echo "`date '+%m/%d %H:%M'` $RunID: $JobMessage" >> completed.log
 
 exit $result
