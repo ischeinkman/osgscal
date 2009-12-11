d5eae09d384062f3f3efb3909643b493884a5346
igor_sfiligoi <igor-sfiligoi@users.noreply.github.com>
2009-12-11 21:31:15 +0000

    Create initial skeleton

---
+++ load_generators/glideTester/trunk/bin/glideTester.py
@@ -0,0 +1,21 @@
+#!/bin/env python
+########################################
+#
+# Description:
+#  This program creates a glidein pool for
+#  use as a large scale parallel test framework
+#
+# Author:
+#  Igor Sfiligoi @ UCSD
+#
+########################################
+
+
+import sys
+
+def main(argv):
+    return
+
+
+if __name__ == "__main__":
+    main(sys.argv)
