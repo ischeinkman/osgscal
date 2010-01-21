21f948535dd5147678b2c33fea63a088ed28eb06
Chris Theissen <ctheissen@gmail.com>
2010-01-21 17:32:32 +0000

    updated code formatting

--- load_generators/glideTester/trunk/bin/glideTester.py
+++ load_generators/glideTester/trunk/bin/glideTester.py
@@ -156,151 +156,152 @@ def run(config):
     try:
         # most of the code goes here
 	
-	# first load the file, so we check it is readable
-	fd = open('parameters.cfg', 'r')
-	try:
-		lines = fd.readlines()
-	finally:
-		fd.close()
+    # first load the file, so we check it is readable
+    fd = open('parameters.cfg', 'r')
+    try:
+        lines = fd.readlines()
+    finally:
+        fd.close()
 
-	# reset the values
-	executable = None
-	arguments = None
-	concurrency = None
-	owner = None
+    # reset the values
+    executable = None
+    arguments = None
+    concurrency = None
+    owner = None
 
-	# read the values
-	for line in lines:
-		line = line.strip()
-		if line[0:1] in ('#',''):
-			continue # ignore comments and empty lines
-		arr = line.split('=',1)
-		if len(arr) != 2:
-			raise RuntimeError, 'Invalid parameter line, missing =: %s'%line
-		key = arr[0].strip()
-		val = arr[1].strip()
-		if key == 'executable':
-			if not os.path.exists(val):
-				raise RuntimeError, "%s '%s' is not a valid executable"%(key,val)
-			executable = val
-		elif key == 'owner':
-			owner=val
-		elif key == 'arguments':
-			arguments = val
-		elif key == 'concurrency':
-			concurrency=val
-	concurrencyLevel = concurrency.split()
+    # read the values
+    for line in lines:
+        line = line.strip()
+        if line[0:1] in ('#',''):
+            continue # ignore comments and empty lines
+        arr = line.split('=',1)
+        if len(arr) != 2:
+            raise RuntimeError, 'Invalid parameter line, missing =: %s'%line
+        key = arr[0].strip()
+        val = arr[1].strip()
+        if key == 'executable':
+            if not os.path.exists(val):
+                raise RuntimeError, "%s '%s' is not a valid executable"%(key,val)
+            executable = val
+        elif key == 'owner':
+            owner=val
+        elif key == 'arguments':
+            arguments = val
+        elif key == 'concurrency':
+            concurrency=val
+    concurrencyLevel = concurrency.split()
 
-	# make sure all the needed values have been read,
-	# and assign defaults, if needed
-	universe = 'vanilla'
-	if executable == None:
-		raise RuntimeError, "executable was not defined!"
-		executable = raw_input("Enter executable: ");
-	transfer_executable = "True"
-	when_to_transfer_output = "ON_EXIT"
-	requirements = '(GLIDEIN_Site =!= "UCSD12") && (Arch =!= "abc")'
-	if owner == None:
-		owner = 'Undefined'
-	notification = 'Never'
+    # make sure all the needed values have been read,
+    # and assign defaults, if needed
+    universe = 'vanilla'
+    if executable == None:
+        raise RuntimeError, "executable was not defined!"
+        executable = raw_input("Enter executable: ");
+    transfer_executable = "True"
+    when_to_transfer_output = "ON_EXIT"
+    requirements = '(GLIDEIN_Site =!= "UCSD12") && (Arch =!= "abc")'
+    if owner == None:
+        owner = 'Undefined'
+    notification = 'Never'
 
-	# Create a testing loop for each concurrency
-	results = []
-	for i in range(0, len(concurrencyLevel), 1):
+    # Create a testing loop for each concurrency
+    results = []
+    for i in range(0, len(concurrencyLevel), 1):
 
-		# request the glideins
-		# we want 10% more glideins than the concurrency level
-		requestedGlideins = int(concurrencyLevel[i])
-		totalGlideins = int(requestedGlideins + .1 * requestedGlideins))
-		gktid.request_glideins(totalGlideins)
+        # request the glideins
+        # we want 10% more glideins than the concurrency level
+        requestedGlideins = int(concurrencyLevel[i])
+        totalGlideins = int(requestedGlideins + .1 * requestedGlideins))
+        gktid.request_glideins(totalGlideins)
 		
-		# now we create the directories for each job and a submit file
-		workingDir = os.getcwd()
-		for k in range(0, len(concurrencyLevel), 1):
-			loop = 0
-			dir1 = workingDir + '/' + 'test' + concurrencyLevel[k] + '/'
-			os.makedirs(dir1)
-			logfile = dir1 + 'test' + concurrencyLevel[k] + '.log'
-			outputfile = 'test' + concurrencyLevel[k] + '.out'
-			errorfile = 'test' + concurrencyLevel[k] + '.err'
-			filename = dir1 + 'submit.condor'
-			FILE=open(filename, "w")
-			FILE.write('universe=' + universe + '\n')
-			FILE.write('executable=' + executable + '\n')
-			FILE.write('transfer_executable=' + transfer_executable + '\n')
-			FILE.write('when_to_transfer_output=' + when_to_transfer_output + '\n')
-			FILE.write('Requirements=' + requirements + '\n')
-			FILE.write('+Owner=' + owner + '\n')
-			FILE.write('log=' + logfile + '\n')
-			FILE.write('output=' +  outputfile + '\n')
-			FILE.write('error=' + errorfile + '\n')
-			FILE.write('notification=' + notification + '\n' + '\n')
-			if arguments != None:
-				FILE.write('Arguments =' + arguments + '\n')
-			for j in range(0, int(concurrencyLevel[k]), 1):
-				FILE.write('Initialdir = ' + 'job' + str(loop) + '\n')
-				FILE.write('Queue' + '\n' + '\n')
-				loop = loop + 1
-			for i in range(0, int(concurrencyLevel[k]), 1):
-				dir2 = dir1 + 'job' + str(i) + '/'
-				os.makedirs(dir2)
-			FILE.close()
+        # now we create the directories for each job and a submit file
+        workingDir = os.getcwd()
+        for k in range(0, len(concurrencyLevel), 1):
+            loop = 0
+            dir1 = workingDir + '/test' + concurrencyLevel[k] + '/'
+            os.makedirs(dir1)
+            logfile = dir1 + 'test' + concurrencyLevel[k] + '.log'
+            outputfile = 'test' + concurrencyLevel[k] + '.out'
+            errorfile = 'test' + concurrencyLevel[k] + '.err'
+            filename = dir1 + 'submit.condor'
+            FILE=open(filename, "w")
+            FILE.write('universe=' + universe + '\n')
+            FILE.write('executable=' + executable + '\n')
+            FILE.write('transfer_executable=' + transfer_executable + '\n')
+            FILE.write('when_to_transfer_output=' + when_to_transfer_output + '\n')
+            FILE.write('Requirements=' + requirements + '\n')
+            FILE.write('+Owner=' + owner + '\n')
+            FILE.write('log=' + logfile + '\n')
+            FILE.write('output=' +  outputfile + '\n')
+            FILE.write('error=' + errorfile + '\n')
+            FILE.write('notification=' + notification + '\n' + '\n')
+            if arguments != None:
+                FILE.write('Arguments =' + arguments + '\n')
+            for j in range(0, int(concurrencyLevel[k]), 1):
+                FILE.write('Initialdir = ' + 'job' + str(loop) + '\n')
+                FILE.write('Queue' + '\n' + '\n')
+                loop = loop + 1
+            for i in range(0, int(concurrencyLevel[k]), 1):
+                dir2 = dir1 + 'job' + str(i) + '/'
+                os.makedirs(dir2)
+            FILE.close()
 
-		# Need to figure out when we have all the glideins
-		# Ask the glidekeeper object
-		finished = "false"
-		while finished != "true":
-			numberGlideins = gktid.get_running_glideins()
-			if numberGlideins = requestedGlideins:
-				finished = "true"
+        # Need to figure out when we have all the glideins
+        # Ask the glidekeeper object
+        finished = "false"
+        while finished != "true":
+            numberGlideins = gktid.get_running_glideins()
+            if numberGlideins = requestedGlideins:
+                finished = "true"
 
-		# Now we begin submission and monitoring
+        # Now we begin submission and monitoring
 		
-		## Need to figure this part out
- 		submission = condorManager.condorSubmitOne(filename)
-		running = "true"
-		while running != "false":	
-			check1 = condorMonitor.CondorQ()
+        ## Need to figure this part out
+        submission = condorManager.condorSubmitOne(filename)
+        running = "true"
+        while running != "false":	
+            check1 = condorMonitor.CondorQ()
 		
-			# Not sure if this is the correct constraint to put on the monitor
-			if check1 == None:
-				running = "false"
+            # Not sure if this is the correct constraint to put on the monitor
+            if check1 == None:
+                running = "false"
 
-		# Need to check log files for when first
-		# job submitted  and last job finished
-		hours = []
-		minutes = []
-		seconds = []
-		logCheck = open(logfile, 'r')
-		try:
-			lines1 = logCheck.readlines()
-		finally:
-			logCheck.close()
-		for line in lines1:
-			line = line.strip()
-			if line[0:1] in ('(','.','U','R','J','C','G'):
-				continue # ignore unwanted text lines
-		        arr1 = line.split(') ',1)
-		        if len(arr1) < 2:
-            			    continue
-        		arr2 = arr1[1].split(' ',2)
-        		time = arr2[1].split(':',2)
-       			hours.append(int(time[0]))
-        		minutes.append(int(time[1]))
-        		seconds.append(int(time[2]))
-		diffHours = (hours[len(hours)-1] - hours[0]) * 3600
-		diffMinutes = (minutes[len(minutes)-1] - minutes[0]) * 60
-		diffSeconds = seconds[len(seconds)-1] - seconds[0]
-		totalTime = diffHours + diffMinutes + diffSeconds
-		final = [totalTime, concurrencyLevel[i]]
-		results.append(final)
+        # Need to check log files for when first
+        # job submitted  and last job finished
+        hours = []
+        minutes = []
+        seconds = []
+        logCheck = open(logfile, 'r')
+        try:
+            lines1 = logCheck.readlines()
+        finally:
+            logCheck.close()
+        for line in lines1:
+            line = line.strip()
+            if line[0:1] in ('(','.','U','R','J','C','G'):
+                continue # ignore unwanted text lines
+            arr1 = line.split(') ',1)
+            if len(arr1) < 2:
+                continue
+            arr2 = arr1[1].split(' ',2)
+            time = arr2[1].split(':',2)
+            hours.append(int(time[0]))
+            minutes.append(int(time[1]))
+            seconds.append(int(time[2]))
+        diffHours = (hours[len(hours)-1] - hours[0]) * 3600
+        diffMinutes = (minutes[len(minutes)-1] - minutes[0]) * 60
+        diffSeconds = seconds[len(seconds)-1] - seconds[0]
+        totalTime = diffHours + diffMinutes + diffSeconds
+        final = [totalTime, concurrencyLevel[i]]
+        results.append(final)
 	
-		# Cleanup all the directories and files made
-		shutil.rmtree(dir1)	
+        # Cleanup all the directories and files made
+        shutil.rmtree(dir1)	
 
-	# Write results to a data file for plotting
+        # Write results to a data file for plotting
 
-#        pass
+        pass
+
     finally:
         gktid.soft_kill()
         gktid.join()
